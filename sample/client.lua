971b116f76b72f415d1707aeb6a8fedb7ed4a7b1
“trumanzhao” <trumanzhao@tencent.com>
2019-05-02 15:53:51 +0800

    完善测试代码

--- sample/client.lua
+++ sample/client.lua
@@ -2,31 +2,64 @@
 
 lbus = require("lbus")
 
-socket_mgr = lbus.create_socket_mgr(10);
+socket_mgr = lbus.create_socket_mgr(10000);
 
-ss = socket_mgr.connect("127.0.0.1", 9999, 2000);
+sessions = {};
 
-ss.on_connect = function(res)
-    print("connect ..."..res);
-    ss.ok = true;
+session_count = 0;
+message_count = 0;
+error_count = 0;
+errors = {};
+
+function init_session(ss)
+    ss.count = 0;
+    ss.on_connect = function(res)
+        if res == "ok" then
+            session_count = session_count + 1; 
+            ss.send("hello", 123);            
+            return;
+        end
+        sessions[ss.token] = nil;        
+    end
+
+    ss.on_recv = function(msg, ...)
+        ss.send(msg, ...);
+        ss.count = ss.count + 1;
+        message_count = message_count + 1;
+
+        if ss.count > 100 then
+            sessions[ss.token] = nil;
+            session_count = session_count - 1;
+            ss.close();        
+        end
+    end
+
+    ss.on_error = function(err)
+        sessions[ss.token] = nil;
+        session_count = session_count - 1;
+        error_count = error_count + 1;
+        if errors[err] == nil then
+            errors[err] = 1;
+            print(err);
+        end        
+    end
+
+    sessions[ss.token] = ss;   
 end
 
-ss.on_recv = function(msg, ...)
-    print("->"..msg);
-end
-
-ss.on_error = function(err)
-    print(err);
-    ss.close();
-end
-
-time = hive.get_time_ms();
+print_time = hive.get_time_ms();
 hive.run = function()
     socket_mgr.wait(100);
+
     local now = hive.get_time_ms();
-    if ss.ok and now > time + 2000 then
-        time = now;
-        ss.send("hello", 123);
+    if now > print_time then
+        print("session_count="..session_count..", message_count="..message_count..", error_count="..error_count);
+        print_time = now + 2000;
+    end
+
+    local ss = socket_mgr.connect("127.0.0.1", 9999, 2000);
+    if ss then
+        init_session(ss);
     end
 end
 
--- sample/server.lua
+++ sample/server.lua
@@ -2,27 +2,49 @@
 
 lbus = require("lbus")
 
-socket_mgr = lbus.create_socket_mgr(1024);
-
-listen_node = socket_mgr.listen("127.0.0.1", 9999);
-
+socket_mgr = lbus.create_socket_mgr(10000);
 sessions = {};
 
-listen_node.on_accept = function(ss)
+listener = socket_mgr.listen("127.0.0.1", 9999);
+
+session_count = 0;
+message_count = 0;
+message_speed = 0;
+error_count = 0;
+errors = {};
+
+listener.on_accept = function(ss)
     ss.on_recv = function(msg, ...)
-        print("recv:" .. msg);
-        ss.send("recved", msg, ...);
+        ss.send(msg, ...);
+        message_count = message_count + 1;
+        message_speed = message_speed + 1;
     end
 
     ss.on_error = function(err)
-        print(err);
         sessions[ss.token] = nil;
+        session_count = session_count - 1;
+        error_count = error_count + 1;
+        if errors[err] == nil then
+            errors[err] = 1;
+            print(err);
+        end
     end
 
     sessions[ss.token] = ss;
+    session_count = session_count + 1;
 end
+
+print_time = hive.get_time_ms();
 
 hive.run = function()
     socket_mgr.wait(100);
+
+    local now = hive.get_time_ms();
+    if now > print_time then
+        local speed = message_speed // 2;
+        print("session_count="..session_count..", message_count="..message_count..", message_speed="..speed);
+        print_time = now + 2000;
+        message_speed = 0;
+    end
 end
 
